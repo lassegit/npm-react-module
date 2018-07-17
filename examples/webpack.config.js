943d23286d6b7df4e47dbe90a088dfdfc955e6fa
Lasse <lassetange@gmail.com>
2018-07-17 15:58:36 +0200

    Adding webpack-server

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,34 +1,37 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, './index.js'),
+  entry: [path.resolve(__dirname, "./index.js")],
   output: {
     path: __dirname,
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
-  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname),
+    port: 9000
+  },
+  devtool: "cheap-module-eval-source-map",
   module: {
     loaders: [
       {
         test: /\.js?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/,
         options: {
-          presets: ['env']
+          presets: ["env"]
         }
       },
       {
         test: /\.css/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
+          fallback: "style-loader",
+          use: "css-loader"
         })
       }
     ]
   },
   watch: true,
-  plugins: [
-    new ExtractTextPlugin('style.css')
-  ]
+  plugins: [new ExtractTextPlugin("style.css")]
 };
